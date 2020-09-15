0ec67d79b829e50fe7a3fd14e07c65261381d93d
pgonin <paul.gonin@suse.com>
2020-09-16 01:01:59 +0200

    Initial version of os-net-delete.sh

---
+++ os-net-delete.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+delete-ports() {
+    echo "Delete all ports"
+    netID="$(openstack network show pgo-net -f json |jq -r ".id")"
+    echo "Network id: $netID"
+    pIDs="$(openstack port list --network $netID -f json | jq -r ".[] | .ID")"
+    for i in $pIDs; do
+      echo "Delete port id: $i"
+        openstack port delete $i
+    done
+}
+
+delete-subnets() {
+    echo "Delete all subnets"
+    sIDs="$(openstack network show $1 -f json | jq -r ".subnets")"
+    for i in $sIDs; do
+        echo "Delete subnet $i"
+        openstack subnet delete $i
+    done
+}
+
+# delete load balancer
+delete-net() {
+    echo "Delete net $1"
+    openstack network delete $1
+}
+
+delete-ports $1
+delete-subnets $1
+delete-net $1
